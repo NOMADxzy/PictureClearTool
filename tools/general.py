7e87d3f194b1e10dfda7fa9fe17863c66d757df9
NOMADxzy <3302309675@qq.com>
2022-04-28 14:51:05 +0800

    添加新标签，自定义标签训练模型

--- tools/general.py
+++ tools/general.py
@@ -13,7 +13,7 @@ if(os.path.exists(settings_file_path)):
     with open(settings_file_path, 'rb') as file:
         settings = pickle.load(file)
         file.close()
-else: settings = {'blur':70,'face':0.5,'rela':0.75,'weight':'中','mincandicate':5}
+else: settings = {'blur':70,'face':0.5,'rela':0.75,'weight':'中','mincandicates':5}
 
 # 加载注册的文件夹
 PathDict = {}
